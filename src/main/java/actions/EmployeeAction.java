76b03106f14ee43ab600239726697d8264acb17d
mizuki-oba <m-oba@fissc.co.jp>
2026-06-12 15:36:04 +0900

    従業員updateアクションの作成

--- src/main/java/actions/EmployeeAction.java
+++ src/main/java/actions/EmployeeAction.java
@@ -97,7 +97,7 @@ public class EmployeeAction extends ActionBase{
                     AttributeConst.DEL_FLAG_FALSE.getIntegerValue());
 
             // アプリケーションスコープからpepper文字列を取得
-            String pepper = getCotextScope(PropertyConst.PEPPER);
+            String pepper = getContextScope(PropertyConst.PEPPER);
 
             // 従業員情報登録
             List<String> errors = service.create(ev, pepper);
@@ -149,7 +149,11 @@ public class EmployeeAction extends ActionBase{
         forward(ForwardConst.FW_EMP_SHOW);
     }
 
-
+    /**
+     * 編集画面を表示する
+     * @throws ServletException
+     * @throws IOException
+     */
     public void edit() throws ServletException, IOException {
         //idを条件に従業員データを取得する
         EmployeeView ev = service.findOne(toNumber(getRequestParam(AttributeConst.EMP_ID)));
@@ -169,7 +173,50 @@ public class EmployeeAction extends ActionBase{
 
     }
 
+    /**
+     * 更新を行う
+     * @throws ServletException
+     * @throws IOException
+     */
+    public void update() throws ServletException, IOException{
 
+        //CSRF対策　tokenのチェック
+        if(checkToken()) {
+            //パラメータの値を元に従業員情報のインスタンスを作成する
+            EmployeeView ev = new EmployeeView(
+                    toNumber(getRequestParam(AttributeConst.EMP_ID)),
+                    getRequestParam(AttributeConst.EMP_CODE),
+                    getRequestParam(AttributeConst.EMP_NAME),
+                    getRequestParam(AttributeConst.EMP_PASS),
+                    toNumber(getRequestParam(AttributeConst.EMP_ADMIN_FLG)),
+                    null,
+                    null,
+                    AttributeConst.DEL_FLAG_FALSE.getIntegerValue());
 
+            //アプリケーションスコープからpepper文字列を取得
+            String pepper = getContextScope(PropertyConst.PEPPER);
 
+            //従業員情報更新
+            List<String>errors = service.update(ev, pepper);
+
+            if(errors.size() > 0) {
+                //更新中にエラーが発生した場合
+
+                putRequestScope(AttributeConst.TOKEN, getTokenId()); //CSRF対策用トークン
+                putRequestScope(AttributeConst.EMPLOYEE, ev); //入力された従業員情報
+                putRequestScope(AttributeConst.ERR, errors); //エラーのリスト
+
+              //編集画面を再表示
+                forward(ForwardConst.FW_EMP_EDIT);
+            } else {
+                //更新中にエラーがなかった場合
+
+                //セッション中に更新完了のフラッシュメッセージを設定
+                putSessionScope(AttributeConst.FLUSH, MessageConst.I_UPDATED.getMessage());
+
+                //一覧画面にリダイレクト
+                redirect(ForwardConst.ACT_EMP, ForwardConst.CMD_INDEX);
+            }
+        }
+    }
 }
