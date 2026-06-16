08e9a5c17f1496b515a9e663ce757679ef053ce9
mizuki-oba <m-oba@fissc.co.jp>
2026-06-16 16:50:12 +0900

    トップページアクションの作成

--- src/main/java/actions/ReportAction.java
+++ src/main/java/actions/ReportAction.java
@@ -219,9 +219,9 @@ public class ReportAction extends ActionBase{
             if(errors.size() > 0) {
                 //更新中にエラーが発生した場合
 
-                putRequestScope(AttributeConst.TOKEN, getTokenId());
-                putRequestScope(AttributeConst.REPORT, rv);
-                putRequestScope(AttributeConst.ERR, errors);
+                putRequestScope(AttributeConst.TOKEN, getTokenId()); //CSRF対策用トークン
+                putRequestScope(AttributeConst.REPORT, rv); //入力された日報情報
+                putRequestScope(AttributeConst.ERR, errors); //エラーのリスト
 
                 //編集画面を表示
                 forward(ForwardConst.FW_REP_EDIT);
